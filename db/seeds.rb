b169d9b0693d7c2f806108fba83856ef475bedd7
rlandows <rlandows@uci.edu>
2017-01-03 10:31:27 -0800

    checkpoint 17 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,9 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(title: "A unique title", body: "A unique body")
+puts "#{Post.count}"
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
