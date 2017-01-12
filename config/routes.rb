dc36b210162cb4819ac75bc6a613fa8954636c7a
rlandows <rlandows@uci.edu>
2017-01-11 19:21:44 -0800

    checkpoint-25-2

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Bloccit::Application.routes.draw do
    end
 
    resources :users, only: [:new, :create]
-   
+
+   post 'users/confirm' => 'users#confirm'
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
