4efc723876a5190028aafea53e198f4b147a549b
rlandows <rlandows@uci.edu>
2017-01-14 12:44:50 -0800

    checkpoint-29

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Bloccit::Application.routes.draw do
     resources :posts, except: [:index]
    end
 
+   resources :posts, only: [] do
+ # #5
+     resources :comments, only: [:create, :destroy]
+   end
+
    resources :users, only: [:new, :create]
 
    resources :sessions, only: [:new, :create, :destroy]
