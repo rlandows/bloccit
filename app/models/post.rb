6aa42b3d5d61278cb894295eac12a261de8f0ba2
rlandows <rlandows@uci.edu>
2017-01-15 15:41:57 -0800

    checkpoint 32

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ActiveRecord::Base
   after_create :create_favorite
 
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
   scope :ordered_by_title, -> {order('title DESC')}
   scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
 
@@ -47,5 +48,5 @@ class Post < ActiveRecord::Base
    def create_favorite
      Favorite.create(post: self, user: self.user)
      FavoriteMailer.new_post(self).deliver_now
-   end 
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Bloccit::Application.routes.draw do
      post '/down-vote' => 'votes#down_vote', as: :down_vote
    end
 
-   resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
    resources :sessions, only: [:new, :create, :destroy]
 
