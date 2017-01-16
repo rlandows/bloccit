e7c8cdf7c344cc26a7119764cb118b8270aa5050
rlandows <rlandows@uci.edu>
2017-01-15 23:52:36 -0800

    checkpoint-32-3

---
+++ spec/factories/comments.rb
@@ -0,0 +1,8 @@
+include RandomData
+FactoryGirl.define do
+    factory :comment do
+    body RandomData.random_paragraph
+    user
+    commentable {create :post}
+   end
+ end
---
+++ spec/factories/votes.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+   factory :votes do
+    value
+    user_id
+    post_id
+  
+   end
+ end
